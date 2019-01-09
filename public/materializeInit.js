da023abd7336d7733aa4a99b75de7484a648ac3b
Cindy Cortes <cindy.indins@gmail.com>
2019-01-09 09:26:12 -0700

    no changes

--- public/materializeInit.js
+++ public/materializeInit.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems);
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('select');
+//   var instances = M.FormSelect.init(elems);
+// });
