f8fb1520c24e41e528a2a42229b4a0ed7e36c01c
Cindy Cortes <cindy.indins@gmail.com>
2019-01-09 18:19:56 -0700

    updated wins in comp controller

--- controllers/competitions_controller.js
+++ controllers/competitions_controller.js
@@ -148,7 +148,7 @@ module.exports = {
           status: 'won',
           isClaimed: false
         }).then((wins)=>{
-          res.render('winners.ejs', {user:req.session.user, participants:results, competition:comp[0], wins:win})
+          res.render('winners.ejs', {user:req.session.user, participants:results, competition:comp[0], wins:wins})
         })
       })
     })
